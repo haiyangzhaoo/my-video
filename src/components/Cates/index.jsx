b43dc9f9bf147da5891016f8870618b601e59c9f
wb-zhy452461 <wb-zhy452461@alibaba-inc.com>
2020-12-01 15:05:47 +0800

    feat: 分类增加下拉折叠功能

--- src/components/Cates/index.jsx
+++ src/components/Cates/index.jsx
@@ -1,21 +1,48 @@
 import React from 'react'
+import { Collapse } from 'antd'
 
 import './index.less'
+
+const { Panel } = Collapse
 
 export default function Cates(props)
 {
   const {data} = props
 
-  return (
-    <div className="cates-container">
-      <h3>{data.title}：</h3>
-      <div className="cates-lists">
-        <span className="active">全部</span>
-        {
-          data.list.map((val, key) => <span key={key}>{val}</span>)
-        }
+  function callback(key) {
+    console.log(key);
+  }
+
+  const itemRender = () => {
+    return (
+      <div className="cates-container">
+        <h3>{data.title}：</h3>
+        <div className="cates-lists">
+          <span className="active">全部</span>
+          {
+            data.list.map((val, key) => <span key={key}>{val}</span>)
+          }
+        </div>
+        {/*data.isMore ? <span>更多</span> : null*/}
       </div>
-      {data.isMore ? <span>更多</span> : null}
-    </div>
+    )
+  }
+
+  return (
+    <React.Fragment>
+      {data.isMore ? (
+        <div className="cates-container">
+          <Collapse onChange={callback} expandIconPosition="right">
+            <Panel header={itemRender()} key="1">
+            <div className="cates-lists">
+              {
+                data.list.map((val, key) => <span key={key}>{val}</span>)
+              }
+            </div>
+            </Panel>
+          </Collapse>
+        </div>
+      ) :itemRender()}
+    </React.Fragment>
   )
 }
