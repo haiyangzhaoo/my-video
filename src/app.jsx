73e58f26b87a36e810d13d1b92026dfa8dd557f1
wb-zhy452461 <wb-zhy452461@alibaba-inc.com>
2020-12-01 17:24:26 +0800

    feat: 增加错误页面

--- src/app.jsx
+++ src/app.jsx
@@ -3,6 +3,7 @@ import {HashRouter, Route, Switch} from 'react-router-dom'
 import Home from '@/pages/Home'
 import Course from '@/pages/Course'
 import Work from '@/pages/Work'
+import ErrorPage from '@/pages/ErrorPage'
 
 export default function router() {
   return (
@@ -11,6 +12,7 @@ export default function router() {
         <Route exact path="/" component={Home} />
         <Route path="/course" component={Course} />
         <Route path="/work" component={Work} />
+        <Route component={ErrorPage} />
       </Switch>
     </HashRouter>
   )
---
+++ src/pages/ErrorPage/index.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+import './index.less'
+
+export default function ErrorPage()
+{
+  return (
+    <div>页面不存在~~~</div>
+  )
+}
