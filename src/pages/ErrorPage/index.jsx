fff12cdcd449aad5b4ad724c9c67160f25dcc7ba
wb-zhy452461 <wb-zhy452461@alibaba-inc.com>
2020-12-02 11:37:31 +0800

    feat: 错误页处理

--- src/pages/ErrorPage/index.jsx
+++ src/pages/ErrorPage/index.jsx
@@ -1,10 +1,21 @@
 import React from 'react'
+import Header from '@/components/Header'
+import Footer from '@/components/Footer'
+import {Button} from 'antd'
 
 import './index.less'
 
 export default function ErrorPage()
 {
   return (
-    <div>页面不存在~~~</div>
+    <div className="error_container">
+      <Header />
+      <div className="error_content">
+        <Button danger size="large" href="/app.html#/">
+          Error Page
+        </Button>
+      </div>
+      <Footer />
+    </div>
   )
 }
